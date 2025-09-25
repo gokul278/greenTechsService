ebf32973d812875b4ec1d8347a2b8add59fdf9a3
gokul278 <gokulhk278@gmail.com>
2025-09-25 08:15:03 +0530

    Update the Sub Trainer User Module

--- model/SubtrainerModel.go
+++ model/SubtrainerModel.go
@@ -11,3 +11,22 @@ type ReqnewSubtrainerRegistrationModel struct {
 	ProfileImage    string `json:"profileImage" mapstructure:"profileImage"`
 	Resume          string `json:"resume" mapstructure:"resume"`
 }
+
+type ReqGetSubtrainerRegistrationModel struct {
+	Id int `json:"id" binding:"required" mapstructure:"id"`
+}
+
+type SubtrainerListModel struct {
+	UserId              int    `json:"refUserId" gorm:"column:refUserId"`
+	RefUserName         string `json:"refUserName" gorm:"column:refUserName"`
+	RefUserStatus       string `json:"refUserStatus" gorm:"column:refUserStatus"`
+	RefUserDOB          string `json:"refUserDOB" gorm:"column:refUserDOB"`
+	RefUserProfile      string `json:"refUserProfile" gorm:"column:refUserProfile"`
+	RefUserCustId       string `json:"refUserCustId" gorm:"column:refUserCustId"`
+	RefUCAddress        string `json:"refUCAddress" gorm:"column:refUCAddress"`
+	RefUCMobileno       string `json:"refUCMobileno" gorm:"column:refUCMobileno"`
+	RefUCMail           string `json:"refUCMail" gorm:"column:refUCMail"`
+	RefSTDWorkExprience string `json:"refSTDWorkExprience" gorm:"column:refSTDWorkExprience"`
+	RefSDTAadhar        string `json:"refSDTAadhar" gorm:"column:refSDTAadhar"`
+	RefSDTResume        string `json:"refSDTResume" gorm:"column:refSDTResume"`
+}
